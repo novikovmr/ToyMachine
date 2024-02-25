b9f3d82486a7cb5d3e6ad6fabbc4d7e6b63198d9
novikovmr <maksim101101@gmail.com>
2024-02-25 18:04:25 +0400

    Добавил AdminUI

---
+++ src/UI/AdminUi.java
@@ -0,0 +1,14 @@
+package UI;
+
+public class AdminUi {
+    public static void getMessage() {
+        System.out.println("\n~~~~~ГЛАВНОЕ МЕНЮ~~~~~");
+        System.out.println("\n~~~~~Выберите нужный пункт~~~~~\n");
+        System.out.println("1 - Посмотреть доступные игрушки");
+        System.out.println("2 - Добавить игрушку");
+        System.out.println("3 - Удалить игрушку");
+        System.out.println("4 - Изменить редкость игрушки");
+        System.out.println("5 - Завершение работы программы");
+
+    }
+}
--- src/UI/UserUI.java
+++ src/UI/UserUI.java
@@ -9,6 +9,8 @@ public class UserUI {
         System.out.println("3 - Посмотреть список выигранных игрушек");
         System.out.println("4 - ИСПЫТАТЬ СУДЬБУ");
         System.out.println("5 - Получить игрушки");
+        System.out.println("6 - Завершение работы программы");
+
     }
     public static void aboutDropChance() {
         System.out.println("У каждой игрушки есть свой уровень редкости.\nCommon - самый высокий шанс выигрыша;\nRare - шанс выпадения 2/10;\nMythical - шанс выпадения 1/200;\nLegendary - шанс выпадения 1/50.\n");
